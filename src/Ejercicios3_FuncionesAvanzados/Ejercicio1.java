f4018dcfb7b555b73c22ea815b54ecb92c98cee1
EHR <hola@>
2025-10-27 20:27:03 +0100

    Add Ejercicio1 a medias

---
+++ src/Ejercicios3_FuncionesAvanzados/Ejercicio1.java
@@ -0,0 +1,9 @@
+package Ejercicios3_FuncionesAvanzados;
+
+// Implementa un método recursivo que determine si un número es primo.
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
