ef71d9bbf83ffa689a1899d3b142744329d0ccc0
EHR <hola@>
2025-11-17 18:30:39 +0100

    Add ejercicio ArrayDeArrays

--- src/Tema2/Arrays/ArrayDeArrays.java
+++ src/Tema2/Arrays/ArrayDeArrays.java
@@ -1,7 +1,32 @@
 package Tema2.Arrays;
 
+import java.util.Arrays;
+
 public class ArrayDeArrays {
     public static void main(String[] args) {
+        //con array simplificado
 
+        int[] cadena1 = {1, 2, 3};
+        int[] cadena2 = {4, 5, 6};
+
+        int[][] conjuncion = {cadena1, cadena2};
+
+        for (int[] conjunto : conjuncion) {
+            System.out.println(Arrays.toString(conjunto));
+        }
+
+        //array
+        int[][] union = new int[2][3];
+        int k = 1;
+
+        for (int i = 0; i < union.length; i++) {
+            for (int j = 0; j < union[i].length; j++) {
+                union[i][j] = k++;
+            }
+        }
+
+        for (int[] uno : union) {
+            System.out.println(Arrays.toString(uno));
+        }
     }
 }
